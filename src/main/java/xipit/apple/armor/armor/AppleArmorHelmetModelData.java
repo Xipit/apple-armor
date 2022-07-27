5f863794d058a0246e5baa4b600c372119d6e0d3
Hanno Witzleb <hanno2604@gmx.de>
2022-07-27 18:41:47 +0200

    [MODEL] model done

--- src/main/java/xipit/apple/armor/armor/AppleArmorHelmetModelData.java
+++ src/main/java/xipit/apple/armor/armor/AppleArmorHelmetModelData.java
@@ -7,27 +7,38 @@ public class AppleArmorHelmetModelData {
         ModelData data = new ModelData();
         ModelPartData root = data.getRoot();
 
-        root.addChild("hat", ModelPartBuilder.create(), ModelTransform.NONE);
-        root.addChild("body", ModelPartBuilder.create(), ModelTransform.NONE);
-        root.addChild("left_arm", ModelPartBuilder.create(), ModelTransform.NONE);
-        root.addChild("right_arm", ModelPartBuilder.create(), ModelTransform.NONE);
-        root.addChild("left_leg", ModelPartBuilder.create(), ModelTransform.NONE);
-        root.addChild("right_leg", ModelPartBuilder.create(), ModelTransform.NONE);
-
         var head = root.addChild(
                 "head",
                 ModelPartBuilder.create(),
                 ModelTransform.NONE);
 
-        //TODO: customize helmet
         head.addChild(
                 "helmet",
                 ModelPartBuilder.create()
                         .uv(0, 0)
                         .mirrored(false)
-                        .cuboid(-4.0F, -32.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(1F)),
+                        .cuboid(-4.0F, -40.0F + 8F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.75F)),
                 ModelTransform.of(0.0F, 24.0F, 0.0F, 0.0F, 0.0F, 0.0F)
         );
+
+        head.addChild(
+                "apple_twig",
+                ModelPartBuilder.create()
+                        .uv(0, 0)
+                        .mirrored(false)
+                        .cuboid(-1.5F, -1.5F, -0.5F, 1.0F, 3.0F, 1.0F, new Dilation(0.0F)),
+                ModelTransform.of(0.5F, -41.5F + 31.5F, 0.0F, 0.0F, 0.0F, -0.2182F)
+        );
+
+        head.addChild(
+                "apple_leaf",
+                ModelPartBuilder.create()
+                        .uv(0, 16)
+                        .mirrored(false)
+                        .cuboid(-1.0F, -1.7F, -1.35F, 5.0F, 3.0F, 1.0F, new Dilation(0.0F)),
+                ModelTransform.of(0.0F, -42.0F + 31.5F, 0.0F,2.2234F, 0.1623F, -0.546F)
+        );
+
 
 
         return data;
--- src/main/java/xipit/apple/armor/util/ModelHandler.java
+++ src/main/java/xipit/apple/armor/util/ModelHandler.java
@@ -7,7 +7,7 @@ import xipit.apple.armor.armor.AppleArmorHelmetModelData;
 import java.util.function.BiConsumer;
 
 public class ModelHandler {
-    public static final EntityModelLayer APPLE_HELMET = RegistryHelper.model(("catear_armor"));
+    public static final EntityModelLayer APPLE_HELMET = RegistryHelper.model("catear_armor");
 
     public static void init(BiConsumer<EntityModelLayer, TexturedModelData> consumer){
         consumer.accept(APPLE_HELMET, TexturedModelData.of(AppleArmorHelmetModelData.getModelData(), 32, 32));
